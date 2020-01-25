b22c2268b614f1148fbb1daec1d5177949bc4dd7
saqib <saqib@SAQIB>
2020-01-25 08:36:49 -0500

    vector with arraylist

--- Collections/src/com/array/Vectr.java
+++ Collections/src/com/array/Vectr.java
@@ -1,12 +1,17 @@
 package com.array;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Vectr {
 
 	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("aaa");
+		al.add("bbb");
+		
 		// default capacity 10
-		Vector<String> v = new Vector<String>(2, 14);
+		Vector<String> v = new Vector<String>(al);
 		System.out.println(v.capacity());
 		v.add("saqib");
 		v.add("ansari");
@@ -22,9 +27,7 @@ public class Vectr {
 		v.add("ansari");
 		v.add("arfa");
 		System.out.println(v.capacity());
-		
-		
-		
+		System.out.println(v);
 
 	}
 
